72cfb26f7980f1e58f8e6af89eae16fd75b55ab4
zloopk09 <zloopk09@gmail.com>
2018-10-12 11:25:22 +0800

    [add]:add splash

---
+++ app/src/main/java/top/zloop/mobile/cornerstone/MyApplication.java
@@ -0,0 +1,12 @@
+package top.zloop.mobile.cornerstone;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/top/zloop/mobile/cornerstone/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package top.zloop.mobile.cornerstone.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.zloop.mobile.cornerstone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
