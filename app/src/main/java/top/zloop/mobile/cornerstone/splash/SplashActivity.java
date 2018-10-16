12ca4aab7277d4ea03b4d96f297c1b53c88632bf
zloopk09 <zloopk09@gmail.com>
2018-10-16 15:23:34 +0800

    [update]:增加splash

--- app/src/main/java/top/zloop/mobile/cornerstone/splash/SplashActivity.java
+++ app/src/main/java/top/zloop/mobile/cornerstone/splash/SplashActivity.java
@@ -1,8 +1,13 @@
 package top.zloop.mobile.cornerstone.splash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import top.zloop.mobile.cornerstone.MainActivity;
 import top.zloop.mobile.cornerstone.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,6 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);  //无title
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //全屏
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
