fdfef793894f1d71a4a9ee9320fb473a48c974c9
zloopk09 <zloopk09@gmail.com>
2018-10-16 17:19:08 +0800

    [add]:底部tab的点击切换

--- app/src/main/java/top/zloop/mobile/cornerstone/MainActivity.java
+++ app/src/main/java/top/zloop/mobile/cornerstone/MainActivity.java
@@ -3,9 +3,16 @@ package top.zloop.mobile.cornerstone;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import top.zloop.mobile.biz.home.HomeFragment;
+import top.zloop.mobile.biz.message.MessageFragment;
+import top.zloop.mobile.biz.search.SearchFragment;
+import top.zloop.mobile.biz.user.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    selectedFragment = HomeFragment.newInstance();
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    selectedFragment = SearchFragment.newInstance();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    selectedFragment = MessageFragment.newInstance();
+                    break;
+                case R.id.navigation_user:
+                    selectedFragment = UserFragment.newInstance();
+                    break;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -39,6 +53,13 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HomeFragment.newInstance());
+        transaction.commit();
+
+
     }
 
 }
