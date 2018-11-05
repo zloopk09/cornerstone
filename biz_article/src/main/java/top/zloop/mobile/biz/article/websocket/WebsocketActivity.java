8a0c3d760cd1194c880f5983de4e7fe264d8af6f
zloopk09 <zloopk09@gmail.com>
2018-11-05 09:27:59 +0800

    [add]:add article library

---
+++ biz_article/src/main/java/top/zloop/mobile/biz/article/websocket/WebsocketActivity.java
@@ -0,0 +1,15 @@
+package top.zloop.mobile.biz.article.websocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.zloop.mobile.biz.article.R;
+
+public class WebsocketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_websocket);
+    }
+}
