4a751a1f58098cacb57068b5f855fc918972a156
zloopk09 <zloopk09@gmail.com>
2018-11-05 09:31:48 +0800

    [add]:add articles

---
+++ biz_article/src/main/java/top/zloop/mobile/biz/article/bottomtabs/BottomTabsActivity.java
@@ -0,0 +1,15 @@
+package top.zloop.mobile.biz.article.bottomtabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.zloop.mobile.biz.article.R;
+
+public class BottomTabsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_tabs);
+    }
+}
---
+++ biz_article/src/main/java/top/zloop/mobile/biz/article/socket/SocketActivity.java
@@ -0,0 +1,15 @@
+package top.zloop.mobile.biz.article.socket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.zloop.mobile.biz.article.R;
+
+public class SocketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket);
+    }
+}
