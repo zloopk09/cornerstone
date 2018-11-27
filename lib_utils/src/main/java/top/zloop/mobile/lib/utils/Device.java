b65ec2cfe0418a7d2d53bf1488b4a4735eb9c8cd
zloopk09 <zloopk09@gmail.com>
2018-11-27 16:45:27 +0800

    [update]:update

---
+++ lib_utils/src/main/java/top/zloop/mobile/lib/utils/Device.java
@@ -0,0 +1,10 @@
+package top.zloop.mobile.lib.utils;
+
+import java.util.Locale;
+
+public class Device {
+    public static final String USER_AGENT = "Mozilla/5.0 (Linux; U; Android "
+            + android.os.Build.VERSION.RELEASE + ";"
+            + Locale.getDefault().toString() + "; " + android.os.Build.DEVICE
+            + "/" + android.os.Build.ID + ")";
+}
