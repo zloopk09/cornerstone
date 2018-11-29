2c9dc5f8983a17f32e1442624319d21df149a63b
zloopk09 <zloopk09@gmail.com>
2018-11-29 09:37:05 +0800

    [update]:update device info

--- lib_utils/src/main/java/top/zloop/mobile/lib/utils/Device.java
+++ lib_utils/src/main/java/top/zloop/mobile/lib/utils/Device.java
@@ -3,7 +3,25 @@ package top.zloop.mobile.lib.utils;
 import java.util.Locale;
 
 public class Device {
-    public static final String USER_AGENT = "Mozilla/5.0 (Linux; U; Android "
+
+    String DeviceModel = android.os.Build.MODEL;
+    String OS_Version = System.getProperty("os.version");
+    String Build_Version = android.os.Build.VERSION.RELEASE;
+    String Device = android.os.Build.DEVICE;
+    String PRODUCT = android.os.Build.PRODUCT;
+    String BRAND = android.os.Build.BRAND;
+    String DISPLAY = android.os.Build.DISPLAY;
+    String CPU_ABI = android.os.Build.CPU_ABI;
+    String CPU_ABI2 = android.os.Build.CPU_ABI2;
+    String UNKNOWN = android.os.Build.UNKNOWN;
+    String HARDWARE = android.os.Build.HARDWARE;
+    String ID = android.os.Build.ID;
+    String MANUFACTURER = android.os.Build.MANUFACTURER;
+    String SERIAL = android.os.Build.SERIAL;
+    String USER = android.os.Build.USER;
+    String HOST = android.os.Build.HOST;
+
+    public static final String USER_AGENT = "Android "
             + android.os.Build.VERSION.RELEASE + ";"
             + Locale.getDefault().toString() + "; " + android.os.Build.DEVICE
             + "/" + android.os.Build.ID + ")";
