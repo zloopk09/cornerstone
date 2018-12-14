d8ac1363c66b23f2cd21dc8a9703f100dcfedb34
zloopk09 <zloopk09@gmail.com>
2018-12-14 17:08:12 +0800

    [update]:增加kotlin写法的plugin

--- buildSrc/src/main/kotlin/top/zloop/gradle/plugins/BuildTimeKotlinPlugin.kt
+++ buildSrc/src/main/kotlin/top/zloop/gradle/plugins/BuildTimeKotlinPlugin.kt
@@ -19,3 +19,20 @@ class BuildTimeKotlinPlugin : Plugin<Project> {
             }
     }
 }
+
+/**
+ * Access the `android` extension of this project. If the project is not an
+ * Android app or library module, this method will throw.
+ */
+val Project.android: BaseExtension
+    get() = extensions.findByName("android") as? BaseExtension
+        ?: error("Project '$name' is not an Android module. Can't " +
+                "access 'android' extension.")
+
+/**
+ * Accesses the app module-specific extensions of an Android module.
+ */
+val BaseExtension.app: AppExtension
+    get() = this as? AppExtension
+        ?: error("Android module is not an app module. Can't " +
+                "access 'android' app extension.")
