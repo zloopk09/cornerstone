6799046baaa9bb47b419b970d0d75138c54b0463
zloopk09 <zloopk09@gmail.com>
2018-12-14 17:03:13 +0800

    [update]:增加kotlin写法的plugin

--- buildSrc/src/main/kotlin/top/zloop/gradle/plugins/BuildTimeKotlinPlugin.kt
+++ buildSrc/src/main/kotlin/top/zloop/gradle/plugins/BuildTimeKotlinPlugin.kt
@@ -2,8 +2,9 @@ package top.zloop.gradle.plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import top.zloop.gradle.tasks.BuildTimeKotlinTask
 
-class MyPlugin : Plugin<Project> {
+class BuildTimeKotlinPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.logger.lifecycle("MyPlugin was successfully applied " +
                 "to your project named '${target.name}'!")
